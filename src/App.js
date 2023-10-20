10ad19da6bdc06f24e362417310ac8003538074f
Learnuel1 <learnueltech@gmail.com>
2023-10-20 22:18:59 +0100

    updated project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {io} from "socket.io-client";
 function App() {
+  const socket = io();
+  const user ={userId: "12sf7834uioioi53424", receiver: "87hjfgr453228"};
+  socket.emit("lock", user);
+
+  socket.on("notify", payload =>{
+    console.log(payload);
+  })
+  
+  const sendLocation = () => {
+    user.location = "afam";
+    console.log(user)
+  socket.emit("location", user);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+    {/* <div className="App"> */}
+      {/* <header className="App-header"> */}
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,9 +30,17 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+      {/* </header> */}
+      <div>
+
+        <button className="btn"  onClick ={(e) =>sendLocation()}>
+              send location
+        </button>
     </div>
+
+      {/* </div> */}
+      </>
   );
 }
 
