0d62c406229a1a96fd35abe14e1ffc87ffeb0684
Learnuel1 <learnueltech@gmail.com>
2023-10-21 05:09:03 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {io} from "socket.io-client";
+
 function App() {
   const socket = io();
   const user ={userId: "12sf7834uioioi53424", receiver: "87hjfgr453228"};
@@ -12,20 +13,16 @@ function App() {
   
   const sendLocation = () => {
     user.location = "afam";
+    socket.emit("location", user);
     console.log(user)
-  socket.emit("location", user);
   }
   return (
     <>
-    
       <div>
-
         <button className="btn"  onClick ={(e) =>sendLocation()}>
               send location
         </button>
     </div>
-
-      {/* </div> */}
       </>
   );
 }
